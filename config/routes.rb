1553ab500dc167f73534889d6e16debc194f864d
dan <daan.forever@gmail.com>
2013-08-04 14:41:41 +0400

    added messages scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Gem1day::Application.routes.draw do
 
-
   devise_for :users
-  root to: redirect('/about.html')
+
+  resources :messages
+
+  root "messages#index"
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130803130102) do
+ActiveRecord::Schema.define(version: 20130803140055) do
+
+  create_table "messages", force: true do |t|
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
