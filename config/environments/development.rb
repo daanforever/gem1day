5bcc1b39964f96a375b5766a1448b41f014f26d6
dan <daan.forever@gmail.com>
2013-08-12 16:15:06 +0400

    changed behaviour on unpermitted_parameters to :raise and removed roles changes from registrations/edit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,6 @@ Gem1day::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_controller.action_on_unpermitted_parameters = :raise
   
 end
